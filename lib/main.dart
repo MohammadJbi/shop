f5d13af61a880657f39d31e8bdf94925d7fc71ce
Mohammadjbi <Mohammadmhdi.jabbari@gmail.com>
2024-06-30 16:34:24 +0330

    remove Globalkey in MatterialApp

--- lib/main.dart
+++ lib/main.dart
@@ -6,8 +6,6 @@ import 'package:apple_shop_application/util/auth_manager.dart';
 import 'package:flutter/material.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 
-GlobalKey<NavigatorState> globalNavigatorKey = GlobalKey<NavigatorState>();
-
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Hive.initFlutter();
@@ -30,7 +28,6 @@ class _MyAppState extends State<MyApp> {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      navigatorKey: globalNavigatorKey,
       home: Scaffold(
         body: (AuthManager.readAuth().isEmpty)
             ? LoginScreen()
